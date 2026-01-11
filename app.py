4c16e1c932631ce52ad06e73c47b498c41ab3056
Rani Kumari <ranischool53@email.com>
2026-01-11 11:45:43 +0530

    db changes

--- app.py
+++ app.py
@@ -8,10 +8,11 @@ CORS(app)  #allows all origin
 # databse connection
 def get_db_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        host=os.environ.get("MYSQLHOST"),
+        user=os.environ.get("MYSQLUSER"),
+        password=os.environ.get("MYSQLPASSWORD"),
+        database=os.environ.get("MYSQLDATABASE"),
+        port=int(os.environ.get("MYSQLPORT"))
     )
 
 # health route
